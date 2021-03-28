2962f549bec61e9c766072bf4d3fc23312c5cc4f
VP <VP>
2021-03-28 23:33:36 +0300

    commit #1

--- GetLocation/ViewController.swift
+++ GetLocation/ViewController.swift
@@ -6,14 +6,35 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet var label: UILabel!
+    var manager: CLLocationManager?
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        label.text = "ready"
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        manager = CLLocationManager()
+        manager?.delegate = self
+        manager?.desiredAccuracy = kCLLocationAccuracyBest
+        manager?.requestWhenInUseAuthorization()
+        manager?.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let first = locations.first else {
+            return
+        }
+        
+        label.text = "\(first.coordinate.longitude) | \(first.coordinate.latitude)"
+    }
 
 }
 
